b49fca3772680a94efe78916ad96fe8fd4cf3477
srii5477 <sridevi_b220555cs@nitc.ac.in>
2026-04-03 11:27:23 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,69 +5,60 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.err.println("Logs from your program will appear here!");
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 9092;
+        try {
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            serverSocket.setSoTimeout(5);
+            // Wait for connection from client.
+            while (true) {
+                clientSocket = serverSocket.accept();
+                DataInputStream in = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+                byte[] inputBytes = new byte[23];
 
-    // TODO: Uncomment the code below to pass the first stage
+                int n = in.read(inputBytes);
+                System.out.println(Arrays.toString(inputBytes));
+                int intVal = ByteBuffer.allocate(2).put(Arrays.copyOfRange(inputBytes, 6, 8)).getShort(0);
 
-     ServerSocket serverSocket = null;
-     Socket clientSocket = null;
-     int port = 9092;
-     try {
-       serverSocket = new ServerSocket(port);
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-       // Wait for connection from client.
-       clientSocket = serverSocket.accept();
-       DataInputStream in = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
-       byte[] inputBytes = new byte[23];
-
-       int n = in.read(inputBytes);
-       System.out.println(Arrays.toString(inputBytes));
-       int intVal = ByteBuffer.allocate(2).put(Arrays.copyOfRange(inputBytes, 6, 8)).getShort(0);
-
-       DataOutputStream dout = new DataOutputStream(clientSocket.getOutputStream());
-       dout.writeInt(19); // msg size
-         dout.writeByte(inputBytes[8]); //corr id
-         dout.writeByte(inputBytes[9]);
-         dout.writeByte(inputBytes[10]);
-         dout.writeByte(inputBytes[11]);
-         if(intVal>=0 && intVal<=4) {
-             dout.writeShort(0);
-         } else {
-             dout.writeShort(35);
-         }
-         dout.writeByte(2); // api_keys array length
-         dout.writeShort(18); //api_key
-         dout.writeShort(0); //min version
-         dout.writeShort(4); // max version
-         dout.writeByte(0); //emtpy tag buffer
-         dout.writeInt(0); //throttle_time_ms
-         dout.writeByte(0); //emtpy tag buffer
-//         dout.writeInt(0);
-//         dout.writeByte(inputBytes[8]);
-//         dout.writeByte(inputBytes[9]);
-//         dout.writeByte(inputBytes[10]);
-//         dout.writeByte(inputBytes[11]);
-//         if(intVal>=0 && intVal<=4) {
-//             dout.writeShort(0);
-//         } else {
-//             dout.writeShort(35);
-//         }
-       dout.flush();
-       dout.close();
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     } finally {
-       try {
-         if (clientSocket != null) {
-           clientSocket.close();
-         }
-       } catch (IOException e) {
-         System.out.println("IOException: " + e.getMessage());
-       }
-     }
-  }
+                DataOutputStream dout = new DataOutputStream(clientSocket.getOutputStream());
+                dout.writeInt(19); // msg size
+                dout.writeByte(inputBytes[8]); //corr id
+                dout.writeByte(inputBytes[9]);
+                dout.writeByte(inputBytes[10]);
+                dout.writeByte(inputBytes[11]);
+                if (intVal >= 0 && intVal <= 4) {
+                    dout.writeShort(0);
+                } else {
+                    dout.writeShort(35);
+                }
+                dout.writeByte(2); // api_keys array length
+                dout.writeShort(18); //api_key
+                dout.writeShort(0); //min version
+                dout.writeShort(4); // max version
+                dout.writeByte(0); //emtpy tag buffer
+                dout.writeInt(0); //throttle_time_ms
+                dout.writeByte(0); //emtpy tag buffer
+                dout.flush();
+                dout.close();
+            }
+        }
+         catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        }
+    }
 }
